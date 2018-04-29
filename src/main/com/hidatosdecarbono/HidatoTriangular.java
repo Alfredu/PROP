46fc5c60d009c100765d0a314a698b20d9fdb451
Aleix Sanchis <aleixsanchis@gmail.com>
2018-04-29 14:21:05 +0200

    Empezada creacion grafo hidato triangular

--- src/main/com/hidatosdecarbono/HidatoTriangular.java
+++ src/main/com/hidatosdecarbono/HidatoTriangular.java
@@ -10,8 +10,25 @@ public class HidatoTriangular extends Hidato {
     }
 
     @Override
-    public ArrayList<Node> getAdyacentes(int i, int j, Node[][] nodo) {
-        return null;
+    public ArrayList<Node> getAdyacentes(int i, int j, Node[][] nodes) {
+        /*
+        Una Celda triangular apunta hacia arriba si se encuentra en una fila par y columna par Ó
+        si se encuentra en una fila impar i una columna impar
+
+        Una Celda triangular apunta hacia abajo si se encuentra en una fila par y columna impar Ó
+        si se encuentra en una fila impar y columna par.
+         */
+        ArrayList<Node> lista = new ArrayList<Node>();
+        int nFilas = this.getNumFilas();
+        int nCols = this.getNumColumnas();
+
+        //Caso 1: Misma fila, casilla de la izquierda
+        if(j>0 && nodes[i][j-1].noEsVacio()) lista.add(nodes[i][j-1]);
+
+        //Caso 2: Misma fila, casilla de la derecha
+        if(j<nCols-1 && nodes[i][j+1].noEsVacio()) lista.add(nodes[i][j+1]);
+
+        return lista;
     }
 
 
