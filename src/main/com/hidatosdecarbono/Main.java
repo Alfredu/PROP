8833d0200444621bfec878b6dfacb77d1d939526
Aleix Sanchis <aleixsanchis@gmail.com>
2018-05-30 12:00:06 +0200

    Añadidos primeros layout de presentacion

--- src/main/com/hidatosdecarbono/Main.java
+++ src/main/com/hidatosdecarbono/Main.java
@@ -3,8 +3,10 @@ package com.hidatosdecarbono;
 public class Main {
 
     public static void main(String[] args){
+        PresentationCTRL presentacio = new PresentationCTRL();
+        presentacio.runLogin();
 
-        UserInterface interficie = new UserInterface();
-        interficie.run();
+        //UserInterface interficie = new UserInterface();
+        //interficie.runLogin();
     }
 }
--- src/presentation/com/hidatosdecarbono/PresentationCTRL.java
+++ src/presentation/com/hidatosdecarbono/PresentationCTRL.java
@@ -1,4 +1,34 @@
 package com.hidatosdecarbono;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class PresentationCTRL {
+
+    private DomainFactory domini;
+    private JFrame frame;
+    PresentationCTRL(){
+        domini = new DomainFactory();
+        frame = new JFrame("HIDATOS DE CARBONO");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public void runLogin(){
+        loadVentana(new LoginLayout(domini.getLogInCTRL(), this).$$$getRootComponent$$$());
+    }
+
+    private void loadVentana(Container cont){
+        frame.setContentPane(cont);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void runMainMenu(){
+        loadVentana(new MainMenu().$$$getRootComponent$$$());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
 }
